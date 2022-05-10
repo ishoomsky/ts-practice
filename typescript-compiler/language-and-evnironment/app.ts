cf947e4641b50894e77c8d426b33cfd3c67092f0
ishumski <ishumski@scand.com>
2022-05-10 14:03:57 +0300

    learn target and lib props

---
+++ typescript-compiler/language-and-evnironment/app.ts
@@ -0,0 +1,6 @@
+const arr = [1,2,3];
+arr.includes(2); //es2016 feature
+
+console.log('123'); // we should add relevant value (e.g. "DOM") to the 'lib' list
+
+Math.random(); // we should add relevant value (e.g. "ES2016") to the 'lib' list
