feb4504292abd74fb5c224815b898d1251d55330
ishumski <ishumski@scand.com>
2022-05-12 12:06:41 +0300

    learn infer

---
+++ types-manipulations/infer/index.ts
@@ -0,0 +1,14 @@
+function runTransaction(transaction : {
+  fromTo: [string, string]
+}) {
+  console.log(transaction);
+}
+
+const transaction: GetFirstArg<typeof runTransaction> = {
+  // fromTo: ['1', '2'] as [string, string], //quick solve
+  fromTo: ['1', '2'],
+}
+
+runTransaction(transaction)
+
+type GetFirstArg<T> = T extends (first: infer First, ...args: any[]) => any ? First : never;
